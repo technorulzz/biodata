dc70c5032b3f4e2fa6d26a3024c671247226a250
technorulzz <m.syahrulramadan@gmail.com>
2020-05-12 19:29:11 +0700

    biodata

---
+++ app/src/main/java/com/example/biodatadiri/ui/profile/ProfileFragment.java
@@ -0,0 +1,30 @@
+//tgl pengerjaan : 11 Mei 2020
+//NIM : 10117250
+//Nama : Muhammad Syahrul Ramadan
+//Kelas : IF-8
+
+package com.example.biodatadiri.ui.profile;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.biodatadiri.R;
+
+public class ProfileFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        return view;
+    }
+}
